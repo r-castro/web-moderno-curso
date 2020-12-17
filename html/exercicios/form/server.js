36c130d0089f0b8cee77e6473381de80912f6628
Rodrigo Castro <ti@megalaser.com.br>
2020-12-17 13:55:10 -0300

    html pack study

---
+++ html/exercicios/form/server.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const { response } = require('express')
+
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.post('/usuarios', (req, res) => {
+    console.log(req.body)
+    res.send('<h1>Parabéns</h1>')
+})
+
+app.post('/usuarios/:id', (req, res) => {
+    console.log(req.params.id)
+    console.log(req.body)
+    res.send('<h1>Parabéns usuário alterado</h1>')
+})
+
+app.listen(3003)
